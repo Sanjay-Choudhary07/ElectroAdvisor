88bc1ea7e22583fdd234660ff5e5bbd979e88d47
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-08-25 23:15:13 +0530

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Header.css';
+import left from '../../assets/left_arrow.png';
+import notification from '../../assets/notification.png';
+import ellipse from '../../assets/ellipse.png';
+import vector from '../../assets/vector.png';
+// import horizontal from '../../assets/horizontal_line.png'
+function Header() {
+  return (
+    // <div className='head'>
+      <div className="header">
+        <div className="header-left">
+          <img src={left} alt="Back Arrow" className="icon arrow" />
+          <span className="title">Product Information</span>
+        </div>
+        <div className="header-right">
+          <div className="date">08 February, Thursday</div>
+          <div>
+            <img src={notification} alt="Notification" className="icon notification" />
+          </div>
+          <div className='user'>
+            <div className="username">Pia Yadav
+            </div>
+            <div className='expert'>Expert
+            </div>
+          </div>
+          <div className='ellip'>
+            <img src={ellipse} alt='Ellipse icon' className='ellipse' />
+          </div>
+          <div className='vector'>
+            <img src={vector} alt="Vector Icon" className="icon vector" />
+          </div>
+        </div>
+      </div>
+     /* <div>
+    <img src={horizontal} className='horizontal'/>
+    </div> */
+    // </div>
+  );
+}
+
+export default Header;
